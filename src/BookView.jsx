db3f5b283cdd84845f00cd1b531b02f5242d6c0e
Kim Seok Jin <kimseokjin6519@gmail.com>
2025-02-22 03:12:06 +0000

    edits

--- src/BookView.jsx
+++ src/BookView.jsx
@@ -3,9 +3,7 @@ import './App.css'
 import { db, collection, getDocs } from "./firebase";
 
 function BookView() {
-
    const [books, setBooks] = useState([]);
-
    const getAllBooks = async () => {
       try {
          const booksRef = collection(db, "books");
@@ -19,28 +17,21 @@ function BookView() {
          console.error("Error getting documents: ", e);
       }
    };
-
    useEffect(() => {
       getAllBooks();
    }, []);
 
    return (
-      <div className="flex flex-col items-center">
-      
+     
+     <div className="flex w-[800px] flex-col items-center border-0 border-gray-200 rounded">
          {books.map((book) => (
-         
-            <div key={book.id}>
-
-               <div className="">{book.title}</div>
-               
-               <div>By {book.author}</div>
-               
-               <div>Published by {book.publisher}</div>
-                              
-               <div>{book.description}</div>
-               
-               <div><img className="w-[300px]" src={`data:image/png;base64,${book.imageBase64}`} alt="Book Image"/></div>
-
+            <div className="flex flex-col" key={book.id}>
+               <div className="flex flex-col items-start text-xl font-medium">{book.title}</div>
+               <div className="flex text-sm text-gray-400">by {book.author}</div>
+               <div className="border-t-2 mt-2 mb-3 border-gray-200"></div>
+               <div className="flex font-light tracking-wider text-justified text-sm mb-4" style={{fontFamily:'Google Sans'}}>{book.description}</div>
+               {/*<div className="flex text-gray-600 mb-2">Published by {book.publisher}</div>*/}
+               {/*<div><img className="w-[300px]" src={`data:image/png;base64,${book.imageBase64}`} alt="Book Image"/></div>*/}
             </div>
 
          ))}
