09e43cbdf8e6ba54e134c8f67c12dd5da69f20f7
Kim Seok Jin <kimseokjin6519@gmail.com>
2025-02-22 03:20:51 +0000

    edits

--- src/BookView.jsx
+++ src/BookView.jsx
@@ -26,10 +26,10 @@ function BookView() {
      <div className="flex w-[800px] flex-col items-center border-0 border-gray-200 rounded">
          {books.map((book) => (
             <div className="flex flex-col" key={book.id}>
-               <div className="flex flex-col items-start text-xl font-medium">{book.title}</div>
+               <div className="flex flex-col items-start text-2xl font-medium">{book.title}</div>
                <div className="flex text-sm text-gray-400">by {book.author}</div>
                <div className="border-t-2 mt-2 mb-3 border-gray-200"></div>
-               <div className="flex font-light tracking-wider text-justified text-sm mb-4" style={{fontFamily:'Google Sans'}}>{book.description}</div>
+               <div className="flex font-light tracking-wider text-justified text-sm text-gray-800 leading-6 mb-4" style={{fontFamily:'Google Sans'}}>{book.description}</div>
                {/*<div className="flex text-gray-600 mb-2">Published by {book.publisher}</div>*/}
                {/*<div><img className="w-[300px]" src={`data:image/png;base64,${book.imageBase64}`} alt="Book Image"/></div>*/}
             </div>
