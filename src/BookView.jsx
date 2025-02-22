36cf6ef518f81ccf2b82e1475e9b81886165d501
Kim Seok Jin <kimseokjin6519@gmail.com>
2025-02-22 09:50:14 +0000

    layout

--- src/BookView.jsx
+++ src/BookView.jsx
@@ -22,20 +22,22 @@ function BookView() {
    }, []);
 
    return (
-     
-     <div className="flex w-[800px] flex-col items-center border-0 border-gray-200 rounded">
+           
+     <div className="flex w-[800px] items-center border-0 border-gray-600 rounded">
          {books.map((book) => (
-            <div className="flex flex-col" key={book.id}>
-               <div className="flex flex-col items-start text-2xl font-medium">{book.title}</div>
-               <div className="flex text-sm text-gray-400">by {book.author}</div>
-               <div className="border-t-2 mt-2 mb-3 border-gray-200"></div>
-               <div className="flex font-light tracking-wider text-justified text-sm text-black leading-6 mb-4" style={{fontFamily:'Google Sans'}}>{book.description}</div>
-               {/*<div className="flex text-gray-600 mb-2">Published by {book.publisher}</div>*/}
-               {/*<div><img className="w-[300px]" src={`data:image/png;base64,${book.imageBase64}`} alt="Book Image"/></div>*/}
+            <div className="flex" key={book.id}>
+               <img className="w-[200px] p-2" src={`data:image/png;base64,${book.imageBase64}`} alt="Book Image"/>
+               <div>
+                  <div className="flex mt-2 flex-col items-start text-2xl font-medium text-gray-600">{book.title}</div>
+                  <div className="flex text-sm text-gray-400">by {book.author}</div>
+                  <div className="border-t-2 mt-2 mb-3 border-gray-200"></div>
+                  <div className="flex font-light tracking-wider text-justified text-sm text-black leading-6 mb-4" style={{fontFamily:'Google Sans'}}>{book.description}</div>
+                  {/*<div className="flex text-gray-600 mb-2">Published by {book.publisher}</div>*/}
+               </div>
+               {/* Column 3 */}
+               <div className="flex"></div>
             </div>
-
          ))}
-
       </div>
    )
 }
